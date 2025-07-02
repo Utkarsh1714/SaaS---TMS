7b434732c14111f858c75d91cbfd151ecd32a100
Utkarsh1714 <utkarshpalav17@gmail.com>
2025-07-02 21:57:04 +0530

    Set Node engine to 20.x

--- backend/routes/employee.route.js
+++ backend/routes/employee.route.js
@@ -1,9 +1,10 @@
 import express from "express";
+const router = express.Router();
+
 import verifyToken from "../middlewares/verifyToken.js";
 import authorizeRole from "../middlewares/authorizeRole.js";
 import { createEmployee, deleteEmployee, getAllEmployees, updateEmployee } from "../controllers/user.controller.js";
 
-const router = express.Router();
 
 router.post('/', verifyToken, authorizeRole("Boss"), createEmployee);
 router.get('/', verifyToken, getAllEmployees);
--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,20 +1,21 @@
-import express from 'express';
-import { addEmployeeToTask, addMilestones, createTask, deleteTask, getTasksByBoss, getTasksByEmployee, getTasksByManager, removeEmployeeFromTask, updateMilestone, updateTaskStatus } from '../controllers/task.controller.js';
-import verifyToken from '../middlewares/verifyToken.js'
-import authorizeRole from '../middlewares/authorizeRole.js'
+    import express from 'express';
+    const router = express.Router();
 
-const router = express.Router();
+    import { addEmployeeToTask, addMilestones, createTask, deleteTask, getTasksByBoss, getTasksByEmployee, getTasksByManager, removeEmployeeFromTask, updateMilestone, updateTaskStatus } from '../controllers/task.controller.js';
+    import verifyToken from '../middlewares/verifyToken.js'
+    import authorizeRole from '../middlewares/authorizeRole.js'
 
-router.post('/create', verifyToken, authorizeRole("Boss"), createTask)
-router.put('/:taskId/employees/add', verifyToken, authorizeRole("Manager"), addEmployeeToTask);
-router.put('/:taskId/employees/remove/:employeeId', verifyToken, authorizeRole("Manager"), removeEmployeeFromTask);
-router.put('/:taskId/status', verifyToken, authorizeRole("Manager"), updateTaskStatus);
-router.post('/:taskId/milestone', verifyToken, authorizeRole("Manager"), addMilestones);
-router.put('/:taskId/milestone/:milestoneIndex', verifyToken, updateMilestone);
-router.delete('/:taskId', verifyToken, authorizeRole("Boss"), deleteTask);
 
-router.get('/boss', verifyToken, getTasksByBoss);
-router.get('/manager', verifyToken, getTasksByManager);
-router.get('/employee', verifyToken, getTasksByEmployee);
+    router.post('/create', verifyToken, authorizeRole("Boss"), createTask)
+    router.put('/:taskId/employees/add', verifyToken, authorizeRole("Manager"), addEmployeeToTask);
+    router.put('/:taskId/employees/remove/:employeeId', verifyToken, authorizeRole("Manager"), removeEmployeeFromTask);
+    router.put('/:taskId/status', verifyToken, authorizeRole("Manager"), updateTaskStatus);
+    router.post('/:taskId/milestone', verifyToken, authorizeRole("Manager"), addMilestones);
+    router.put('/:taskId/milestone/:milestoneIndex', verifyToken, updateMilestone);
+    router.delete('/:taskId', verifyToken, authorizeRole("Boss"), deleteTask);
 
-export default router;
+    router.get('/boss', verifyToken, getTasksByBoss);
+    router.get('/manager', verifyToken, getTasksByManager);
+    router.get('/employee', verifyToken, getTasksByEmployee);
+
+    export default router;
