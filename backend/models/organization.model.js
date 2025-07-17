7b06ea7ed0e5236d1a9a5a0ed301c20caea7575a
Utkarsh1714 <utkarshpalav17@gmail.com>
2025-07-17 23:16:43 +0530

    Add password strength checker in registration page

--- backend/models/organization.model.js
+++ backend/models/organization.model.js
@@ -4,6 +4,7 @@ const OrganizationSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        unique: true,
     },
     gstin: {
         type: String,
--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,32 +1,92 @@
-import { useState } from "react";
+import { useId, useMemo, useState } from "react";
 import DepartmentSelect from "../components/DepartmentSelect";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
 import { toast } from "react-hot-toast";
+import { CheckIcon, EyeIcon, EyeOffIcon, XIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 const Registration = () => {
   const [departments, setDepartments] = useState([]);
+  const [password, setPassword] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
+  const [logoUrl, setLogoUrl] = useState("");
+  const [formFields, setFormFields] = useState({
+    name: "",
+    email: "",
+    contactNumber: "",
+    companyName: "",
+    gstin: "",
+    address: "",
+    city: "",
+    state: "",
+    pincode: "",
+    country: "",
+    websiteUrl: "",
+  });
+
   const Navigate = useNavigate();
   const { login } = useAuth();
+  const id = useId();
+
+  const toggleVisibility = () => setIsVisible((prev) => !prev);
+
+  // Password strength check logic
+  const checkStrength = (pass) => {
+    const requirements = [
+      { regex: /.{8,}/, text: "At least 8 characters" },
+      { regex: /[0-9]/, text: "At least 1 number" },
+      { regex: /[a-z]/, text: "At least 1 lowercase letter" },
+      { regex: /[A-Z]/, text: "At least 1 uppercase letter" },
+    ];
+    return requirements.map((req) => ({
+      met: req.regex.test(pass),
+      text: req.text,
+    }));
+  };
+
+  const strength = checkStrength(password);
+  const strengthScore = useMemo(() => {
+    return strength.filter((req) => req.met).length;
+  }, [strength]);
+
+  const getStrengthColor = (score) => {
+    if (score === 0) return "bg-border";
+    if (score <= 1) return "bg-red-500";
+    if (score <= 2) return "bg-orange-500";
+    if (score === 3) return "bg-amber-500";
+    return "bg-emerald-500";
+  };
+
+  const getStrengthText = (score) => {
+    if (score === 0) return "Enter a password";
+    if (score <= 2) return "Weak password";
+    if (score === 3) return "Medium password";
+    return "Strong password";
+  };
+
+  // ✅ Check if all required fields are filled
+  const isFormValid =
+    Object.values(formFields).every((val) => val.trim() !== "") &&
+    password.trim() !== "" &&
+    departments.length > 0;
+
+  const handleChange = (e) => {
+    setFormFields((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
 
     const formValues = {
-      username: formData.get("name"),
-      email: formData.get("email"),
-      contactNo: formData.get("contactNumber"),
-      password: formData.get("password"),
-      companyName: formData.get("companyName"),
-      gstin: formData.get("gstin"),
-      address: formData.get("address"),
-      city: formData.get("city"),
-      state: formData.get("state"),
-      pincode: formData.get("pincode"),
-      country: formData.get("country"),
-      logoUrl: formData.get("logoUrl"),
+      ...formFields,
+      password,
+      logoUrl,
       websiteUrl: formData.get("websiteUrl"),
       departments: departments.map((dept) => dept.value),
     };
@@ -38,9 +98,9 @@ const Registration = () => {
         { withCredentials: true }
       );
 
-      login(res.data.user)
+      login(res.data.user);
 
-      Navigate('/')
+      Navigate("/");
     } catch (error) {
       console.error("Error registering organization:", error);
       toast(error?.response?.data?.message || "Registration failed");
@@ -67,19 +127,25 @@ const Registration = () => {
             <h1 className="text-2xl">Personal details</h1>
             <div className="w-full flex items-center justify-center gap-8">
               <label className="block text-left mt-4 w-full">
-                <span className="text-gray-700">Your Name</span>
+                <span className="text-gray-700">Your Name<span className="text-red-500 pl-1">*</span></span>
                 <input
                   name="name"
                   type="text"
+                  value={formFields.name}
+                  onChange={handleChange}
+                  placeholder="Enter your full name"
                   className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
                   required
                 />
               </label>
               <label className="block text-left mt-4 w-full">
-                <span className="text-gray-700">Email Address</span>
+                <span className="text-gray-700">Email Address<span className="text-red-500 pl-1">*</span></span>
                 <input
                   name="email"
                   type="email"
+                  value={formFields.email}
+                  onChange={handleChange}
+                  placeholder="Enter your email address"
                   className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
                   required
                 />
@@ -87,22 +153,76 @@ const Registration = () => {
             </div>
             <div className="w-full flex items-center justify-center gap-8">
               <label className="block text-left mt-4 w-full">
-                <span className="text-gray-700">Contact Number</span>
+                <span className="text-gray-700">Contact Number<span className="text-red-500 pl-1">*</span></span>
                 <input
                   name="contactNumber"
                   type="number"
+                  value={formFields.contactNumber}
+                  onChange={handleChange}
+                  placeholder="Ex. +91 1234567890"
                   className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
                   required
                 />
               </label>
               <label className="block text-left mt-4 w-full">
-                <span className="text-gray-700">Set Password</span>
-                <input
-                  name="password"
-                  type="password"
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
-                  required
-                />
+                <span className="text-gray-700">Set Password<span className="text-red-500 pl-1">*</span></span>
+                <div className="relative mt-2">
+                  <Input
+                    id={id}
+                    className="pe-9"
+                    placeholder="Password"
+                    type={isVisible ? "text" : "password"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <button
+                    className="absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center"
+                    type="button"
+                    onClick={toggleVisibility}
+                    aria-label={isVisible ? "Hide password" : "Show password"}
+                  >
+                    {isVisible ? (
+                      <EyeOffIcon size={16} />
+                    ) : (
+                      <EyeIcon size={16} />
+                    )}
+                  </button>
+                </div>
+
+                {/* Strength Bar */}
+                <div className="bg-border mt-3 mb-2 h-1 w-full overflow-hidden rounded-full">
+                  <div
+                    className={`h-full ${getStrengthColor(
+                      strengthScore
+                    )} transition-all duration-500`}
+                    style={{ width: `${(strengthScore / 4) * 100}%` }}
+                  ></div>
+                </div>
+
+                {/* Strength Text */}
+                <p className="text-sm mb-2">
+                  {getStrengthText(strengthScore)}. Must contain:
+                </p>
+
+                {/* Requirements */}
+                <ul className="space-y-1.5">
+                  {strength.map((req, index) => (
+                    <li key={index} className="flex items-center gap-2">
+                      {req.met ? (
+                        <CheckIcon size={16} className="text-emerald-500" />
+                      ) : (
+                        <XIcon size={16} className="text-gray-400" />
+                      )}
+                      <span
+                        className={`text-xs ${
+                          req.met ? "text-emerald-600" : "text-gray-500"
+                        }`}
+                      >
+                        {req.text}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
               </label>
             </div>
           </div>
@@ -116,19 +236,23 @@ const Registration = () => {
           <h1 className="text-2xl">Organization Details</h1>
           <div className="w-full flex items-center justify-center gap-8">
             <label className="block text-left mt-4 w-full">
-              <span className="text-gray-700">Company Name</span>
+              <span className="text-gray-700">Company Name<span className="text-red-500 pl-1">*</span></span>
               <input
                 name="companyName"
                 type="text"
+                value={formFields.companyName}
+                onChange={handleChange}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
                 required
               />
             </label>
             <label className="block text-left mt-4 w-full">
-              <span className="text-gray-700">GSTIN Number</span>
+              <span className="text-gray-700">GSTIN Number<span className="text-red-500 pl-1">*</span></span>
               <input
                 name="gstin"
                 type="text"
+                value={formFields.gstin}
+                onChange={handleChange}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
                 required
               />
@@ -136,19 +260,23 @@ const Registration = () => {
           </div>
           <div className="w-full flex items-center justify-center gap-8">
             <label className="block text-left mt-4 w-full">
-              <span className="text-gray-700">Address</span>
+              <span className="text-gray-700">Address<span className="text-red-500 pl-1">*</span></span>
               <input
                 name="address"
                 type="text"
+                value={formFields.address}
+                onChange={handleChange}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
                 required
               />
             </label>
             <label className="block text-left mt-4 w-full">
-              <span className="text-gray-700">City</span>
+              <span className="text-gray-700">City<span className="text-red-500 pl-1">*</span></span>
               <input
                 name="city"
                 type="text"
+                value={formFields.city}
+                onChange={handleChange}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
                 required
               />
@@ -156,19 +284,23 @@ const Registration = () => {
           </div>
           <div className="w-full flex items-center justify-center gap-8">
             <label className="block text-left mt-4 w-full">
-              <span className="text-gray-700">State</span>
+              <span className="text-gray-700">State<span className="text-red-500 pl-1">*</span></span>
               <input
                 name="state"
                 type="text"
+                value={formFields.state}
+                onChange={handleChange}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
                 required
               />
             </label>
             <label className="block text-left mt-4 w-full">
-              <span className="text-gray-700">Pincode</span>
+              <span className="text-gray-700">Pincode<span className="text-red-500 pl-1">*</span></span>
               <input
                 name="pincode"
                 type="number"
+                value={formFields.pincode}
+                onChange={handleChange}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
                 required
               />
@@ -176,10 +308,12 @@ const Registration = () => {
           </div>
           <div className="w-full flex items-center justify-center gap-8">
             <label className="block text-left mt-4 w-full">
-              <span className="text-gray-700">Country</span>
+              <span className="text-gray-700">Country<span className="text-red-500 pl-1">*</span></span>
               <input
                 name="country"
                 type="text"
+                value={formFields.country}
+                onChange={handleChange}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
               />
             </label>
@@ -188,21 +322,34 @@ const Registration = () => {
               <input
                 name="websiteUrl"
                 type="text"
+                value={formFields.websiteUrl}
+                onChange={handleChange}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
               />
             </label>
           </div>
           <div className="w-full flex items-center justify-center gap-8">
             <label className="block text-left mt-4 w-full">
-              <span className="text-gray-700">Upload Company Logo (URL)</span>
+              <span className="text-gray-700">
+                Upload Company Logo (URL)<span className="text-red-500 pl-1">*</span>
+              </span>
               <input
                 name="logoUrl"
                 type="text"
+                value={logoUrl}
+                onChange={(e) => setLogoUrl(e.target.value)}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-yellow-400"
               />
             </label>
-            <div className="flex items-center justify-center bg-red-400 mt-5">
-              <img src="" alt="Logo Preview" className="w-[200px] h-[200px]" />
+            <div className="flex items-center justify-center mt-5">
+              <img
+                src={
+                  logoUrl ||
+                  "https://img.icons8.com/?size=100&id=114320&format=png&color=000000"
+                }
+                alt="Logo Preview"
+                className="w-[200px] h-[200px] object-contain border rounded"
+              />
             </div>
           </div>
           <div className="w-full flex items-center justify-center gap-8">
@@ -215,7 +362,12 @@ const Registration = () => {
           <div className="w-full flex justify-center mt-8">
             <button
               type="submit"
-              className="px-6 py-2 bg-yellow-400 text-white font-semibold rounded-md hover:bg-yellow-500"
+              disabled={!isFormValid}
+              className={`px-6 py-2 font-semibold rounded-md ${
+                isFormValid
+                  ? "bg-yellow-400 text-white hover:bg-yellow-500"
+                  : "bg-gray-300 text-gray-500 cursor-not-allowed"
+              }`}
             >
               Register
             </button>
