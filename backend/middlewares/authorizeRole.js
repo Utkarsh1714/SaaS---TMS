f5cbe4d4dfe3833c022dc7fad0067a604dcf71f2
Utkarsh1714 <utkarshpalav17@gmail.com>
2025-09-17 18:40:10 +0530

    refactor: clean up and simplify authorizeRole middleware; remove commented-out code

--- backend/middlewares/authorizeRole.js
+++ backend/middlewares/authorizeRole.js
@@ -1,16 +1,3 @@
-// const authorizeRole = (role) => {
-//     return (req, res, next) => {
-//         if (req.user.role !== role) {
-//             return res.status(403).json({
-//                 message: `Access denied: Unauthorized role`,
-//             })
-//         }
-//         next();
-//     }
-// }
-
-// export default authorizeRole;
-
 const authorizeRole = (...allowedRoles) => {
   return (req, res, next) => {
     if (!allowedRoles.includes(req.user.role)) {
