778d539253494cc2569ad06490671481da1e55b9
Utkarsh1714 <utkarshpalav17@gmail.com>
2025-09-22 01:00:04 +0530

    removing the domain property

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -160,10 +160,13 @@ export const login = async (req, res) => {
 
   res
     .cookie("token", token, {
+      // httpOnly: false,
+      // secure: process.env.NODE_ENV === 'production',
+      // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       httpOnly: false,
       secure: process.env.NODE_ENV === 'production', // 🔑 Must be true for production
       sameSite: process.env.NODE_ENV === 'production' ? "none" : "lax", // 🔑 Required with `secure: true` for cross-site cookies
-      domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : 'localhost', // 🔑 This ensures the cookie is shared across all subdomains
+      // domain: process.env.NODE_ENV === 'production' ? '.onrender.com' : 'localhost',
     })
     .json({ message: "Login successful", user: safeUser });
 };
